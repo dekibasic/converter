1e467360ca72df59776ee9472395a4b616fb1b05
dejanbasic <dejan555@gmail.com>
2019-04-14 12:54:55 +0200

    Initial ommit

---
+++ index.py
@@ -0,0 +1,16 @@
+print("Welcome in converter. Here you can convert kilometres in miles!")
+
+km_to_mile = 0.62137119223733
+
+while True:
+    km = int(input("Enter number of km: "))
+    print(km * km_to_mile)
+
+    repeat = input("Do you want to convert again [y/n]?")
+    if repeat == "y":
+        print(km)
+    elif repeat == "n":
+        print("Thank you for using my converter.")
+        break
+    else:
+        print("Error, unknown operation")
